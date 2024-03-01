71056ab81d41b77611018ce398e8f7861377452a
WambuguPeter <gitiropeter4@gmail.com>
2024-03-01 14:19:00 +0300

    Adding a Photo

--- src/app/store.js
+++ src/app/store.js
@@ -4,12 +4,9 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 import { userApi } from "../features/register/userApi.js";
 import { postsApi } from "../features/posts/postApi.js";
 import { friendsApi } from "../features/friends/friendsApi.js";
-<<<<<<< Updated upstream
 import { eventsApi } from "../features/events/eventsApi.js";
 import { videosApi } from "../features/videos/videosApi.js";
-=======
 import { photoApi } from "../features/photos/photoApi.js";
->>>>>>> Stashed changes
 
 export const store = configureStore({
   reducer: {
@@ -17,24 +14,18 @@ export const store = configureStore({
     [userApi.reducerPath]: userApi.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
     [friendsApi.reducerPath]: friendsApi.reducer,
-<<<<<<< Updated upstream
     [eventsApi.reducerPath]: eventsApi.reducer,
     [videosApi.reducerPath]: videosApi.reducer,
-=======
     [photoApi.reducerPath]: photoApi.reducer,
->>>>>>> Stashed changes
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       userApi.middleware,
       postsApi.middleware,
       friendsApi.middleware,
-<<<<<<< Updated upstream
       eventsApi.middleware,
-      videosApi.middleware
-=======
+      videosApi.middleware,
       photoApi.middleware,
->>>>>>> Stashed changes
     ),
 });
 
